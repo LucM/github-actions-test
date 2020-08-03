a49b0ea72a4badc44ec35d5f64ed61e72c9408a6
Luc MOMAL <luc.momal@gmail.com>
2020-08-03 18:17:23 +0200

    fix ut

--- package1/test.js
+++ package1/test.js
@@ -1,5 +1,8 @@
 describe("This is a test", () => {
   it("should run a simple test", () => {
-    expect(true).toBe(false)
+    expect(true).toBe(true)
+  })
+  it("should run a simple test2", () => {
+    expect(true).toBe(true)
   })
 })
